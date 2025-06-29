7c2db05132cd133fc588d59600a497cecc9e8d83
Javyer2003 <javierclaudiomiranda@gmail.com>
2025-06-29 01:56:32 -0300

    Empezando proyecto final, express, carpetas controllers, models, routes

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+
+//el metodo listen recibe dos argumentos, el puerto y una funcion callback
+app.listen(PORT,() => (console.log(`Servidor corriendo en http://localhost:${PORT}`)));
+
+app.get('/', (req, resp)=> resp.send("Hola Mundo desde  Express"));
