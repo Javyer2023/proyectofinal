e792aa205ae44952b550d381c70c07b20260fa1e
Javyer2023 <javierclaudiomiranda@gmail.com>
2025-07-01 22:18:43 -0300

    modifiqué el index.js

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ const PORT = 3000;
 //el metodo listen recibe dos argumentos, el puerto y una funcion callback
 app.listen(PORT,() => (console.log(`Servidor corriendo en http://localhost:${PORT}`)));
 
-app.get('/', (req, resp)=> resp.send("Hola Mundo desde  Express"));
+app.get('/', (req, resp)=> resp.send(`<h1>Hola Mundo desde  Express<h1>`));
