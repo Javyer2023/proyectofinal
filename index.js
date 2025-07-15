683c7518ad0269ad825fb94be172aa636fa2253a
Javyer2003 <javierclaudiomiranda@gmail.com>
2025-07-15 02:11:27 -0300

    data.js .env variables de entorno

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors";
+import 'dotenv/config';
 
 const app = express();
+app.use(cors());
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 
 //el metodo listen recibe dos argumentos, el puerto y una funcion callback
 app.listen(PORT,() => (console.log(`Servidor corriendo en http://localhost:${PORT}`)));
